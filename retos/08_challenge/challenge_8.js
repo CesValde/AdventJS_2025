57d274f7bb1a62f36bec76853aab67e51991fbc6
CesValde <valderramacesar2002@gmail.com>
2025-12-08 18:30:27 -0300

    Dia 8
    .

---
+++ retos/08_challenge/challenge_8.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} toy - The toy to find the first unique one letter
+ * @returns {string} The first unique letter in the toy
+ */
+function findUniqueToy(toy) {
+   // Code here
+   const lower = toy.toLowerCase()
+
+   for (let i = 0; i < toy.length; i++) {
+      const char = toy[i]
+      const lowerChar = lower[i]
+
+      if (lower.indexOf(lowerChar) === lower.lastIndexOf(lowerChar)) {
+         return char
+      }
+   }
+
+   return ""
+}
